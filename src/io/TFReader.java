a11ab1d7f3b66c5421de392a6fa560585a5e5b3c
davexpt <davex.pt@gmail.com>
2011-01-30 19:33:05 +0000

    Global read in the TF reader

--- src/io/TFReader.java
+++ src/io/TFReader.java
@@ -6,13 +6,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+import util.EmailDataset;
+import util.EmailMessage;
+
 
 
 
 public class TFReader {
 	private String filename;
-	private int numSpam;
-	private int numHam;
+	
 	
 	public TFReader(String filename){
 		this.filename = filename;
@@ -20,12 +22,9 @@ public class TFReader {
 	}
 	
 	
-	public List<HashMap<Integer, Integer>> trainRead() throws FileNotFoundException{
-		LinkedList<HashMap<Integer,Integer>> maps = new LinkedList<HashMap<Integer,Integer>>();
-		maps.add(new HashMap<Integer, Integer>());
-		maps.add(new HashMap<Integer, Integer>());
+	public EmailDataset read() throws FileNotFoundException{
 		//fist item spam second ham
-		
+		List<EmailMessage> messages = new LinkedList<EmailMessage>();
 		//read the file
 		Scanner sc = new Scanner(new FileReader(filename));
 		
@@ -37,189 +36,38 @@ public class TFReader {
 			
 			//System.out.println("line separated: "+Integer.parseInt(pairs[0]));
 			
-			if(Integer.parseInt(pairs[0]) == 1){
-				currentHashMap = maps.getFirst();
-				numSpam++;
-			}
-			else if(Integer.parseInt(pairs[0]) == -1){
-				currentHashMap = maps.getLast();
-				numHam++;
-			}
+			int k = 0;
+			int classification = Integer.parseInt(pairs[0]);
+			if(classification == 1 || classification == -1)
+				k = 1;//if classification is present start extracting tokens from index 1 of pairs
+					  //usefull as a flag to choose the EmailMessage constructor
 			
-			String[] integers = null;
-			for(int i=1; i<pairs.length; i++){
-				 integers = pairs[i].split(":");
-				 int n1 = Integer.parseInt(integers[0]);
-				 int n2 = Integer.parseInt(integers[1]);
-				 //System.out.println("pairs: "+n1+":"+n2);
-				 
-				 
-				 int ocurr = 0;
-				 if(currentHashMap.containsKey(n1)){
-					 ocurr = currentHashMap.get(n1);
-					 ocurr += n2;
-				 }
-				 
-				 currentHashMap.put(n1, ocurr);
-				 
-			}
-				
-			
-			
-			
-			
-		}
-		
-		sc.close();
-		return maps;
-		
-	}
-	
-	
-	
-	
-	
-	public List<HashMap<Integer, Integer>> crossRead() throws FileNotFoundException{
-		LinkedList<HashMap<Integer,Integer>> maps = new LinkedList<HashMap<Integer,Integer>>();
-		maps.add(new HashMap<Integer, Integer>());
-		maps.add(new HashMap<Integer, Integer>());
-		//fist item spam second ham
-		
-		//read the file
-		Scanner sc = new Scanner(new FileReader(filename));
-		
-		HashMap<Integer, Integer> currentHashMap = null;
-		while(sc.hasNextLine()){
-			String line = sc.nextLine();
-			//System.out.println("current line:" + line);
-			String[] pairs = line.split(" ");
-			
-			
-			if(Math.random() > 0.5){//subtrain set
-				
-				
-				if(Integer.parseInt(pairs[0]) == 1){
-					currentHashMap = maps.get(0);
-					numSpam++;
-				}
-				else if(Integer.parseInt(pairs[0]) == -1){
-					currentHashMap = maps.get(1);
-					numHam++;
-				}
-				
-				String[] integers = null;
-				for(int i=1; i<pairs.length; i++){
-					 integers = pairs[i].split(":");
-					 int n1 = Integer.parseInt(integers[0]);
-					 int n2 = Integer.parseInt(integers[1]);
-					 //System.out.println("pairs: "+n1+":"+n2);
-					 
-					 
-					 int ocurr = 0;
-					 if(currentHashMap.containsKey(n1)){
-						 ocurr = currentHashMap.get(n1);
-						 ocurr += n2;
-					 }
-					 
-					 currentHashMap.put(n1, ocurr);
-					 
-				}
-					
-			}//end subtrain set
-			else{
-				currentHashMap = new HashMap<Integer, Integer>();
-				maps.add(currentHashMap);
-				
-				String[] integers = null;
-				for(int i=1; i<pairs.length; i++){
-					 integers = pairs[i].split(":");
-					 int n1 = Integer.parseInt(integers[0]);
-					 int n2 = Integer.parseInt(integers[1]);
-					 //System.out.println("pairs: "+n1+":"+n2);
-					 
-			
-					 currentHashMap.put(n1, n2);
-					 
-				}
-				
-				
-			}
-				
-				
-			
-		}
-		
-		sc.close();
-		return maps;
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public List<HashMap<Integer, Integer>> untaggedRead() throws FileNotFoundException{
-		LinkedList<HashMap<Integer,Integer>> maps = new LinkedList<HashMap<Integer,Integer>>();
-		
-		//fist item spam second ham
-		
-		//read the file
-		Scanner sc = new Scanner(new FileReader(filename));
-		
-		HashMap<Integer, Integer> currentHashMap = null;
-		while(sc.hasNextLine()){
-			String line = sc.nextLine();
 			currentHashMap = new HashMap<Integer, Integer>();
-			maps.add(currentHashMap);
-			//System.out.println("current line:" + line);
-			String[] pairs = line.split(" ");
-			
-			//System.out.println("line separated: "+Integer.parseInt(pairs[0]));
-			
-			
 			
 			String[] integers = null;
-			for(int i=0; i<pairs.length; i++){
+			for(int i=k; i<pairs.length; i++){
 				 integers = pairs[i].split(":");
 				 int n1 = Integer.parseInt(integers[0]);
-				 int n2 = Integer.parseInt(integers[1]);
-				 //System.out.println("pairs: "+n1+":"+n2);
-				 
-				 
+				 int n2 = Integer.parseInt(integers[1]);				 
 				 currentHashMap.put(n1, n2);
-				 
 			}
-				
 			
-			
-			
-			
+			if(k == 1)//the line had a class
+				messages.add(new EmailMessage(classification, currentHashMap));
+			else//line was untagged (no classification information)
+				messages.add(new EmailMessage(currentHashMap));	
 		}
 		
 		sc.close();
-		return maps;
-		
-		
+		return new EmailDataset(messages);
 		
 	}
 	
 	
 	
 	
-	public int getNumSpam(){
-		return numSpam;
-		
-	}
 	
-	
-	public int getNumHam(){
-		return numHam;
-		
-	}
+
+
 
 }
